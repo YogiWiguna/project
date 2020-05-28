257fcc1055010eaad04fbbdd2e1958c32e84388d
YogiWiguna <58981369+YogiWiguna@users.noreply.github.com>
2020-05-28 17:05:37 +0800

    membenarkan coding
    kesalahan ketik

--- GameList/app/src/main/java/com/example/gamelist/MainActivity.java
+++ GameList/app/src/main/java/com/example/gamelist/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        recyclerView.findViewById(R.id.gl_id);
+        recyclerView=findViewById(R.id.gl_id);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
